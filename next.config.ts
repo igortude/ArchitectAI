b01fa856020ce981b47b81541ac51e4372bc1acf
Igor Tude <igortude@hotmail.com>
2026-04-22 23:09:10 -0300

    fix vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  
+  // Solução para o bug do Turbopack no Next 16
+  turbopack: {
+    root: process.cwd(), // Isso vai gerar algo como: /home/igor/Documentos/GIT/ArchitectAI_v1.0.0
+  },
 };
 
 export default nextConfig;
